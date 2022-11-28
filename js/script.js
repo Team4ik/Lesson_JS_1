1aa0e8e550d5d8599b7aa53a8075e018aa9ab6d5
Team4ik <113092784+Team4ik@users.noreply.github.com>
2022-11-28 23:03:00 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+`use strict`;
+
+//Домашка JS (урок 02 - синтаксис)
+
+
+//Способи написання фрази Я вчу JS
+//Спосіб_1
+console.log('Я вчу JS');
+//Спосіб_2
+let person = 'Я';
+let doing = 'вчу';
+let object = `JS`;
+let phrase = `${person} ${doing} ${object}`;
+console.log(phrase);
+//Спосіб_3
+console.log(person + " " + doing + " " + object);
+
+
+//Домашка JS (урок 03 - синтаксис)
+
+const COLOR_EYES = 15 + 5;
+
+let user;
+let userName = `Вася`;
+user = userName;
+console.log(user);
+
+console.log(COLOR_EYES)
+
+
